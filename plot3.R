d5da1964840dddca565088523e30841952f44399
cherishine <chenxizf@gmail.com>
2022-06-27 14:08:56 +0800

    Revise position of plot3's legend

--- plot3.R
+++ plot3.R
@@ -16,7 +16,7 @@ plot(data$datetime, data$Sub_metering_1, type="n", xlab="", ylab="Energy sub met
 lines(data$datetime, data$Sub_metering_1, col="black")
 lines(data$datetime, data$Sub_metering_2, col="red")
 lines(data$datetime, data$Sub_metering_3, col="blue")
-legend("topright", lty=c(1,1,1), col=c("black","red","blue"), 
+legend("top", lty=c(1,1,1), col=c("black","red","blue"), 
        legend=c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, "plot3.png")
 dev.off()
